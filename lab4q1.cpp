48889fdd827c0fb4c4a5a0c0a8535e135a58c6eb
Rnazx <42196798+Rnazx@users.noreply.github.com>
2018-08-22 01:23:30 -0400

    Add files via upload

---
+++ lab4q1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+float cml;
+double ml=cml/100,kml=cml/100000; //declare the variables//initialize the variables
+
+
+//print to enter the variables
+cout<<" Enter the length in centimeters "<<endl;
+cin>>cml;
+cout<<" The length in meters is "<<ml<<"meters"<<endl;
+cout<<" The length in kilometers is "<<kml<<"kilometers"<<endl;
+return 0;
+}
+
