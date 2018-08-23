96b3d8af540da4f0eee95ee32c7a183a7c5bc157
Rnazx <42196798+Rnazx@users.noreply.github.com>
2018-08-23 12:56:32 +0000

    Update lab4q2.cpp

--- lab4q2.cpp
+++ lab4q2.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main()
 {
 	float celsius,fahrenheit;                                                                                  //defining variables
-	cout <<"Write what is your temperature in celsius to covert into fahrenheit ";                          // assigning value of celsius for conversion
+	cout <<"Write your temperature in celsius  ";                          // assigning value of celsius for conversion
 	cin >> celsius;
 	fahrenheit =(celsius*9)/5+32;                                                                              // formula for conversion of temprature
         cout <<"Temperature conversion from celsius to fahrenheit is :"<<fahrenheit<<endl;
