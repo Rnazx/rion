e2e7e2eb28a328df3758060fc7a6fee681b5a5b2
Rnazx <42196798+Rnazx@users.noreply.github.com>
2018-09-07 14:46:45 +0530

    Add files via upload

---
+++ lab5Q-8.cpp
@@ -0,0 +1,13 @@
+#include <iostream>                           //adding library
+using namespace std;
+
+      int main(){                            //declaring variable
+      int i=1;                               //conditions
+      while (i <= 100){
+      if(i % 2 == 0){
+     cout <<i<<endl;
+}
+     i++;
+}
+ return 0;                                    //checks code is correct and return value 0
+} 
