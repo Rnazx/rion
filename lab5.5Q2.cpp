c594cb6642415af42d5c6e698b88fd94d25db6e5
Rnazx <42196798+Rnazx@users.noreply.github.com>
2018-09-13 22:08:34 +0530

    Add files via upload

---
+++ lab5.5Q2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>				//to display star pattern
+/*
+****		
+*  *	
+*  *
+**** */								
+using namespace std;
+int main(){
+int i,j,n;
+cout<<" Enter the number of rows \n";
+cin>>n;
+for (i=0;i<n;i++){
+cout<<endl;
+for (j
+		//declare variables
