544462087f3ab64ddda321ee73a80471d65f9f67
Rnazx <42196798+Rnazx@users.noreply.github.com>
2018-09-24 18:24:59 +0530

    Update lab6Q2a.cpp

--- lab6Q2a.cpp
+++ lab6Q2a.cpp
@@ -4,7 +4,7 @@ using namespace std;              //including library
 
 	//Write a function that has two parameters.
 	int func(int a, int b){
-		int sum = a + b;           //their sum.
+		int sum = a + b;           //their sum
 		return sum;
 	}
 
@@ -15,7 +15,7 @@ using namespace std;              //including library
 			cin >>c >>d;
 			   //Tell user the sum
 			cout<<"The sum of the numbers "<<c<< " and " <<d<<" is "<< 
-			//Call function for numbers as arguments
+			//Call function for numbers as arguments.
 		    func(c,d) <<endl;
 		
 return 0;
