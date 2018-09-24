dc3dd0ba9e022d40610b9de9d57d1acdd16c606a
Rnazx <42196798+Rnazx@users.noreply.github.com>
2018-09-24 18:38:24 +0530

    Update lab6Q4b.cpp

--- lab6Q4b.cpp
+++ lab6Q4b.cpp
@@ -1,4 +1,4 @@
-#include <iostream>                 //including library
+#include <iostream>                 //including library.
 using namespace std;
 
 	void funcR(int a, int b, int &c){                      //Functions with parameters and reference parameter.
@@ -12,10 +12,10 @@ using namespace std;
 
 //Driver function
 int main(){   
-	int p,q,m;            //Declaring variables  
+	int p,q,m;            //Declaring variables. 
     	cout << "Give the two numbers to get the lesser of them " ;     //Ask the terminal for two numbers.
     	 cin>>p>>q;
-    	funcR(p,q,m);               //call the arguments
+    	funcR(p,q,m);               //call the arguments.
    	 	 cout<<"The minimum of the two numbers " <<p <<" and " <<q <<" is " <<m<<endl;            //Print the minimum
     	
 return 0;
