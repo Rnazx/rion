db5a3218f627e15dabaece92cc5fd69a8f1d5dd2
Rnazx <42196798+Rnazx@users.noreply.github.com>
2018-10-05 21:14:21 +0530

    Add files via upload

---
+++ 7.cpp
@@ -0,0 +1,31 @@
+//1. Write a C++ program to find power of any number using recursion.
+#include<iostream>
+using namespace std;
+//define fun power 2 var
+		int pow(int b,int p)
+		{
+		
+			if(p>1){
+			return b*pow(b,p-1);//write the recurring relation
+			}
+			else  {
+			return 1;
+			}
+	
+		}	
+		int main(){
+			int base,power,num;
+		cout<<" enter base\n";
+		cin>>base;
+		cout<<" enter power\n";
+		cin>>power;
+		num=pow(base,power);
+		cout<<" the number is "<<num;
+		return 0;
+		}
+
+//write the main function
+//
+	
+	
+
